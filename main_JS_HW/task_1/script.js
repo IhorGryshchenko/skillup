424dc160a232e8f1c5d9b3451f003d5596dd14dd
IhorGryshchenko <shaman5324@gmail.com>
2022-12-28 16:58:14 +0100

    add main_JS_HW and task_1 and task_2

---
+++ main_JS_HW/task_1/script.js
@@ -0,0 +1,10 @@
+const Name = 'protective field generator';
+let price = 1000;
+
+console.log(`${ Name} ${price} credits`
+)
+
+price = 2000;
+
+console.log (`${ Name } ${price } credits`)
+ 
---
+++ main_JS_HW/task_2/script.js
@@ -0,0 +1,9 @@
+const total = 10;
+const ordered = 20;
+
+if(ordered >  total){
+    console.log("На складі недостатньо товарів")
+} else if(ordered <= total){
+    console.log( "Замовлення оформлено, з вами зв'яжеться менеджер")
+} 
+ 
